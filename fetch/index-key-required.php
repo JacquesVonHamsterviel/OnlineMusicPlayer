43d34ff6565d6eab98f87dc5e71220fd7f2dc729
Jacques von Hämsterviel <59958130+JacquesVonHamsterviel@users.noreply.github.com>
2022-12-05 00:03:48 +0000

    fetch-php-version
    fetch-php-version

--- fetch/index-key-required.php
+++ fetch/index-key-required.php
@@ -1,9 +1,11 @@
 <?php
-if (isset($_GET["target"])){
+if (isset($_GET["key"])&&isset($_GET["target"])){
+    if($_GET["key"]=="WKPjMZ9BtpHUwAV"){
         $url=base64_decode($_GET["target"]);
         $options  = array('http' => array('user_agent' => $_SERVER['HTTP_USER_AGENT']));
         $context  = stream_context_create($options);
         $contents = file_get_contents($url, false, $context);
         echo $contents;
-        }else{echo "Access Denied with error: argument error.";}
+    }else{echo "Access Denied with error: key error.";}
+}else{echo "Access Denied with error: argument error.";}
 ?>
